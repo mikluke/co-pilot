693ff1492ee808932f1ce94934faef516331e190
Vladislav Mikitich <vmikitich@gmail.com>
2023-05-27 03:05:23 +0300

    add slice.Unique

--- slice/slice.go
+++ slice/slice.go
@@ -1,5 +1,7 @@
 package slice
 
+import "github.com/mikluke/co-pilot/convert"
+
 func Contains[T comparable](s []T, v T) bool {
 	for i := range s {
 		if s[i] == v {
@@ -8,4 +10,16 @@ func Contains[T comparable](s []T, v T) bool {
 	}
 
 	return false
+}
+
+func Unique[T comparable](s []T) []T {
+	m := make(map[T]struct{}, len(s))
+
+	for _, v := range s {
+		m[v] = struct{}{}
+	}
+
+	return convert.Map2Slice(m, func(k T, _ struct{}) T {
+		return k
+	})
 }
